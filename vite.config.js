ca5dd80d030c6fd936660a0ab50509b2685d5545
v2ray <2282688304@qq.com>
2025-09-14 05:11:23 +0000

    Fixed console forwarding.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import tailwindcss from "@tailwindcss/vite"
 import {consoleForwardPlugin} from "vite-console-forward-plugin"
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: "/front-page",
-    plugins: [react(), tailwindcss(), consoleForwardPlugin()],
-    server: {host: true, port: 12345, allowedHosts: true},
+export default defineConfig(({command, mode}) => {
+    const isDev = command === "serve" || mode === "development";
+    console.log(`Development mode: ${isDev}`);
+    return {
+        base: "/front-page",
+        plugins: [
+            react(),
+            tailwindcss(),
+            isDev && consoleForwardPlugin(),
+        ].filter(Boolean),
+        server: {host: true, port: 12345, allowedHosts: true},
+    }
 })
